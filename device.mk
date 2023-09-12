403542b7523e5ff8c3c77fddcb8b3a977636a718
Matsvei Niaverau <matvej2002@gmail.com>
2023-09-12 08:52:36 +0000

    xaga: Build PowerOffAlarm

--- device.mk
+++ device.mk
@@ -346,6 +346,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
+# PowerOffAlarm
+PRODUCT_PACKAGES += \
+    PowerOffAlarm
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
