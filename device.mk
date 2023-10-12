a86fd07374e279f2107ecbd3f0355917f242b1dd
ChrisCatto <chriscatto08@gmail.com>
2023-10-12 17:34:18 +0300

    xaga: Drop XiaomiParts for now
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- device.mk
+++ device.mk
@@ -453,10 +453,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
-
-# Xiaomi Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
     
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/xaga/xaga-vendor.mk)
